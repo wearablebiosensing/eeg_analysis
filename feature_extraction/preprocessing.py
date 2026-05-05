7dcfdce5999c3d564b0b83dcf990cb36852e32a9
sadhushehjar <45221445+sadhushehjar@users.noreply.github.com>
2026-05-05 11:38:11 -0400

    demo updates

--- feature_extraction/preprocessing.py
+++ feature_extraction/preprocessing.py
@@ -17,13 +17,13 @@ def preprocess_eeg(eeg_data: np.ndarray, fs: float, lowcut=1, highcut=45, order=
     b, a = scipy.signal.butter(order, [low, high], btype='band')
     eeg_data_filtered = scipy.signal.lfilter(b, a, eeg_data)
     
-    # ==========================
-    # ASR
-    # ==========================
-    # (optional) make sure your asr is only fitted to clean parts of the data
+    # # ==========================
+    # # ASR
+    # # ==========================
+    # # (optional) make sure your asr is only fitted to clean parts of the data
     pre_cleaned, _ = clean_windows(eeg_data_filtered, fs, max_bad_chans=0.1)
 
-    # fit the asr-in literature the cutoff is a value between 10 and 30
+    # # fit the asr-in literature the cutoff is a value between 10 and 30
     M, T = asr_calibrate(pre_cleaned, fs, cutoff=asr_cutoff)
     
     # apply it
